825b0ddbb92fbeceea64d664a251270d2ee86547
Richard Piazza <richard@richardpiazza.com>
2018-01-31 13:06:46 -0600

    Missing Enum descriptions; Corrected 'broadcast' in MusicAlbumRelease

--- Sources/SOSwiftVocabulary/Gender.swift
+++ Sources/SOSwiftVocabulary/Gender.swift
@@ -1,5 +1,7 @@
 import Foundation
 
+/// Gender of the person.
+/// While http://schema.org/Male and http://schema.org/Female may be used, text strings are also acceptable for people who do not identify as a binary gender.
 public enum Gender: String, GenderOrText {
     case female = "Female"
     case male = "Male"
