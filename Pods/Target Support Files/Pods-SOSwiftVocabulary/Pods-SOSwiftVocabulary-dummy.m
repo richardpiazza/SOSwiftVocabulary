573da24b4941f3da829011d99c344e14497cbc25
Richard Piazza <richard@richardpiazza.com>
2018-01-31 09:07:09 -0600

    Deintegrated Cocoapods (wasn't needed)

--- Pods/Target Support Files/Pods-SOSwiftVocabulary/Pods-SOSwiftVocabulary-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SOSwiftVocabulary : NSObject
-@end
-@implementation PodsDummy_Pods_SOSwiftVocabulary
-@end
--- Pods/Target Support Files/Pods-SOSwiftVocabularyTests/Pods-SOSwiftVocabularyTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SOSwiftVocabularyTests : NSObject
-@end
-@implementation PodsDummy_Pods_SOSwiftVocabularyTests
-@end
